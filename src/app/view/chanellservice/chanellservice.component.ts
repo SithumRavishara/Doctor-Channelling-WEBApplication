bf0c97e40188027975bd83af5219312da4dc48c6
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-12-08 20:42:53 +0530

    Code updated

---
+++ src/app/view/chanellservice/chanellservice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chanellservice',
+  templateUrl: './chanellservice.component.html',
+  styleUrls: ['./chanellservice.component.css']
+})
+export class ChanellserviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
